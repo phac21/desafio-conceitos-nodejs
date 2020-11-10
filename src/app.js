2acf75fe65c63c889cf7cf3e01a4d7f7656ef917
Pedro Henrique <pedro.semear@gmail.com>
2020-11-10 17:15:26 -0300

    criação das rotas get, post, put

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,15 +11,44 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  // const { id, title, url, techs, likes } = request.query;
+
+  // const results = title
+  //   ? projects.filter(project.title.includes(title))
+  //   : projects;
+
+  return response.json(repositories);
 });
+  
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+  const repository = {  id: uuid(), title, url, techs, likes: 0  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+
+  if (repositoryIndex < 0){
+    return response.status(400).json({ error: "Project not found"});
+  }
+
+  const repository = {
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositoryIndex] = repository;
+  
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
